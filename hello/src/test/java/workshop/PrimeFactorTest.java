177afe8803fc35479f6bf597e52d7ea0863ce209
up1 <somkiat.p@gmail.com>
2023-10-05 16:57:07 +0700

    Refactor test

--- hello/src/test/java/workshop/PrimeFactorTest.java
+++ hello/src/test/java/workshop/PrimeFactorTest.java
@@ -15,8 +15,7 @@ class PrimeFactorTest {
     @Test
     @DisplayName("Input 2 => 2")
     void case01() {
-        List<Integer> expected = new ArrayList<>();
-        expected.add(2);
+        List<Integer> expected = generateData(2);
         PrimeFactor p = new PrimeFactor();
         List<Integer> actualResult = p.process(2);
         assertEquals(expected, actualResult);
@@ -25,9 +24,7 @@ class PrimeFactorTest {
     @Test
     @DisplayName("Input 4 => 2,2")
     void case02() {
-        List<Integer> expected = new ArrayList<>();
-        expected.add(2);
-        expected.add(2);
+        List<Integer> expected = generateData(2, 2);
         PrimeFactor p = new PrimeFactor();
         List<Integer> actualResult = p.process(4);
         assertEquals(expected, actualResult);
@@ -36,10 +33,7 @@ class PrimeFactorTest {
     @Test
     @DisplayName("Input 8 => 2,2,2")
     void case03() {
-        List<Integer> expected = new ArrayList<>();
-        expected.add(2);
-        expected.add(2);
-        expected.add(2);
+        List<Integer> expected = generateData(2, 2, 2);
         PrimeFactor p = new PrimeFactor();
         List<Integer> actualResult = p.process(8);
         assertEquals(expected, actualResult);
@@ -48,26 +42,27 @@ class PrimeFactorTest {
     @Test
     @DisplayName("Input 16 => 2,2,2,2")
     void case04() {
-        List<Integer> expected = new ArrayList<>();
-        expected.add(2);
-        expected.add(2);
-        expected.add(2);
-        expected.add(2);
+        List<Integer> expected = generateData(2, 2, 2, 2);
         PrimeFactor p = new PrimeFactor();
         List<Integer> actualResult = p.process(16);
         assertEquals(expected, actualResult);
     }
 
-//    @Test
-//    @DisplayName("Input 16 => 2,2,2,2")
-//    void case04() {
-//        List<Integer> expected = generateData(2, 2, 2, 2);
-//        PrimeFactor p = new PrimeFactor();
-//        List<Integer> actualResult = p.process(16);
-//        assertEquals(expected, actualResult);
-//    }
-//
-//    private List<Integer> generateData(int ... datas) {
-//        return Arrays.stream(datas).boxed().collect(Collectors.toList());
-//    }
+    @Test
+    @DisplayName("Input 32 => 2,2,2,2,2")
+    void case05() {
+        List<Integer> expected = generateData(2, 2, 2, 2, 2);
+        PrimeFactor p = new PrimeFactor();
+        List<Integer> actualResult = p.process(32);
+        assertEquals(expected, actualResult);
+    }
+
+    private List<Integer> generateData(int ... datas) {
+        List<Integer> list = new ArrayList<>();
+        for (int data : datas) {
+            Integer i = data;
+            list.add(i);
+        }
+        return list;
+    }
 }
