98e768ff6850f1690a4d68f91e147795a970bc57
up1 <somkiat.p@gmail.com>
2023-10-05 16:12:14 +0700

    Add workshop

---
+++ hello/src/main/java/workshop/PrimeFactor.java
@@ -0,0 +1,14 @@
+package workshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactor {
+
+    List<Integer> process(int input) {
+        List<Integer> output = new ArrayList<>();
+        // TODO : next
+        return output;
+    }
+
+}
