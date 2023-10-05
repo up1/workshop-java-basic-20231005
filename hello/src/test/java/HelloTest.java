33a935ce58bfc087741ed0cbc3a1017e09e3ae70
up1 <somkiat.p@gmail.com>
2023-10-05 11:20:56 +0700

    Demo with unit test

--- hello/src/test/java/HelloTest.java
+++ hello/src/test/java/HelloTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,6 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class HelloTest {
 
     @Test
-    void sayHi() {
+    @DisplayName("เรากำลังจะทดสอบเรื่องอะไร ....")
+    void case01() {
+        Hello hello = new Hello();
+        String result = hello.sayHi("somkiat");
+        assertEquals("Hello, somkiat", result);
+    }
+
+    @Test
+    @DisplayName("เรากำลังจะทดสอบเรื่องอะไร ....")
+    void case02() {
+        Hello hello = new Hello();
+        String result = hello.sayHi("somkiat2");
+        assertEquals("Hello, somkiat2", result);
     }
 }
