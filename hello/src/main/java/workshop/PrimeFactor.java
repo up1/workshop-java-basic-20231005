e66c7671a7d1c9755e2bfe5e0cd5735adfffdb3a
up1 <somkiat.p@gmail.com>
2023-10-05 16:26:58 +0700

    Fix case 2

--- hello/src/main/java/workshop/PrimeFactor.java
+++ hello/src/main/java/workshop/PrimeFactor.java
@@ -7,7 +7,11 @@ public class PrimeFactor {
 
     List<Integer> process(int input) {
         List<Integer> output = new ArrayList<>();
-        output.add(input);
+        output.add(2);
+        int remain = input/2;
+        if(remain != 1) {
+            output.add(2);
+        }
         return output;
     }
 
