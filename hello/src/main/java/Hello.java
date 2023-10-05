a90018630df94f859fa7bf6fa62c2fdca4932392
up1 <somkiat.p@gmail.com>
2023-10-05 11:53:26 +0700

    Remove warning

--- hello/src/main/java/Hello.java
+++ hello/src/main/java/Hello.java
@@ -1,10 +1,4 @@
 public class Hello {
-    public static void main(String[] args) {
-        Hello hello = new Hello();
-        String result = hello.sayHi("somkiat");
-        System.out.println(result);
-    }
-
     String sayHi(String name) {
         if ("x".equals(name)) {
             return "New req";
