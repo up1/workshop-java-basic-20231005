7b728cd7b847774d2be54a0506706582b2994602
up1 <somkiat.p@gmail.com>
2023-10-05 14:19:54 +0700

    Object classs + override toString()

--- hello/src/main/java/oop/Employee.java
+++ hello/src/main/java/oop/Employee.java
@@ -8,8 +8,15 @@ enum EmployeeType {
 
 class Demo2 {
     public static void main(String[] args) {
-        Employee employee = new Employee(); // Default Constructor
-        employee.doSth();
+        Employee e1 = new Employee(); // Default Constructor
+        Employee e2 = new Employee();
+        if(e1.equals(e2)) {
+            System.out.println("OK");
+        } else {
+            System.out.println("NOT OK");
+        }
+        System.out.println(e1);
+        System.out.println(e2);
     }
 }
 public class Employee {
@@ -28,6 +35,14 @@ public class Employee {
     public Employee(Integer id, EmployeeType type) {
         this.id = id;
         this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", type=" + type +
+                '}';
     }
 
     // Behaviors or methods
