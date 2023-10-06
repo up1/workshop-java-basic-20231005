309f96a2dbe7ce6831a84a904fac4f1de475f36b
up1 <somkiat.p@gmail.com>
2023-10-06 14:47:33 +0700

    Use mockito

---
+++ hello/src/test/java/workshop/oop/IdGeneratorServiceWithMockitoTest.java
@@ -0,0 +1,43 @@
+package workshop.oop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class IdGeneratorServiceWithMockitoTest {
+
+    @Mock
+    private Random random;
+
+    @Test
+    @DisplayName("ทำการ generate id = Hello 6")
+    void case01() {
+        // Initial dependency
+        when(random.nextInt(anyInt())).thenReturn(6);
+        IdGeneratorService service = new IdGeneratorService();
+        service.setRandom(random);
+        String actual = service.process();
+        assertEquals("Hello 6", actual);
+    }
+
+    @Test
+    @DisplayName("ทำการ generate id = Hello 10")
+    void case02() {
+        // Initial dependency
+        when(random.nextInt(10)).thenReturn(10);
+        IdGeneratorService service = new IdGeneratorService();
+        service.setRandom(random);
+        String actual = service.process();
+        assertEquals("Hello 10", actual);
+    }
+}
