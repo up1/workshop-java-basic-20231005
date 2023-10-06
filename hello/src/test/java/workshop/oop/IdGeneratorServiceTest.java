f2aa276fd923c3609017d70b68c3ecc01e700e33
up1 <somkiat.p@gmail.com>
2023-10-06 14:27:35 +0700

    Create mock class

--- hello/src/test/java/workshop/oop/IdGeneratorServiceTest.java
+++ hello/src/test/java/workshop/oop/IdGeneratorServiceTest.java
@@ -7,21 +7,39 @@ import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+class MockRandom extends Random {
+    private final int number;
+
+    public MockRandom(int number) {
+        this.number = number;
+    }
+    @Override
+    public int nextInt(int bound) {
+        return this.number;
+    }
+}
+
 class IdGeneratorServiceTest {
 
     @Test
     @DisplayName("ทำการ generate id = Hello 6")
     void case01() {
         // Initial dependency
-        Random random = new Random(){
-            @Override
-            public int nextInt(int bound) {
-                return 6;
-            }
-        }; // Real
+        Random random = new MockRandom(6); // Mock
         IdGeneratorService service = new IdGeneratorService();
         service.setRandom(random);
         String actual = service.process();
         assertEquals("Hello 6", actual);
+    }
+
+    @Test
+    @DisplayName("ทำการ generate id = Hello 10")
+    void case02() {
+        // Initial dependency
+        Random random = new MockRandom(10); // Mock
+        IdGeneratorService service = new IdGeneratorService();
+        service.setRandom(random);
+        String actual = service.process();
+        assertEquals("Hello 10", actual);
     }
 }
