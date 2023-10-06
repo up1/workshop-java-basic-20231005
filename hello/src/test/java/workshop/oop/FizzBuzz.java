117d54721ee24e32d7068959620bb7e1235b08c3
up1 <somkiat.p@gmail.com>
2023-10-06 16:45:17 +0700

    Refactor with SOLID

--- hello/src/test/java/workshop/oop/FizzBuzz.java
+++ hello/src/test/java/workshop/oop/FizzBuzz.java
@@ -1,19 +1,81 @@
 package workshop.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
 
     public String say(int input) {
-        if(input % 15 == 0) {
-            return "FizzBuzz";
+        List<MyRule> rules = new ArrayList<>();
+        rules.add(new FizzBuzzRule());
+        rules.add(new FizzRule());
+        rules.add(new BuzzRule());
+        rules.add(new SCBRule());
+
+        for (MyRule rule : rules) {
+            if(rule.check(input)) {
+                return rule.say();
+            }
         }
 
-        if(input % 3 == 0) {
-            return "Fizz";
-        }
-        if(input % 5 == 0) {
-            return "Buzz";
-        }
         return "" + input;
     }
 
+}
+
+interface MyRule {
+    boolean check(int input);
+    String say();
+}
+
+class FizzBuzzRule implements MyRule{
+
+    @Override
+    public boolean check(int input) {
+        return input % 15 == 0;
+    }
+
+    @Override
+    public String say() {
+        return "FizzBuzz";
+    }
+}
+
+class FizzRule implements MyRule{
+
+    @Override
+    public boolean check(int input) {
+        return input % 3 == 0;
+    }
+
+    @Override
+    public String say() {
+        return "Fizz";
+    }
+}
+
+class BuzzRule implements MyRule{
+
+    @Override
+    public boolean check(int input) {
+        return input % 5 == 0;
+    }
+
+    @Override
+    public String say() {
+        return "Buzz";
+    }
+}
+
+class SCBRule implements MyRule{
+
+    @Override
+    public boolean check(int input) {
+        return input % 7 == 0;
+    }
+
+    @Override
+    public String say() {
+        return "SCB";
+    }
 }
